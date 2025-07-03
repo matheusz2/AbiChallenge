ae2297a92530ad98dc7b67b5067f6a7188eed126
Matheus Ortiz <matheus.ortiz@outlook.com.br>
2025-07-03 00:34:21 -0300

    Commit - Ajuste dos Contratos

---
+++ backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/CreateSale/CreateSaleProfile.cs
@@ -0,0 +1,18 @@
+﻿using Ambev.DeveloperEvaluation.Application.Users.CreateUser;
+using Ambev.DeveloperEvaluation.WebApi.Features.Users.CreateUser;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.CreateSale
+{
+    public class CreateSaleProfile : Profile
+    {
+        /// <summary>
+        /// Initializes the mappings for CreateUser feature
+        /// </summary>
+        public CreateSaleProfile()
+        {
+            CreateMap<CreateUserRequest, CreateUserCommand>();
+            CreateMap<CreateUserResult, CreateUserResponse>();
+        }
+    }
+}
---
+++ backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/CreateSale/CreateSaleRequest.cs
@@ -0,0 +1,22 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Enums;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.CreateSale
+{
+    public class CreateSaleRequest
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier of the customer associated with the sale.
+        /// </summary>
+        public Guid CustomerId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the unique identifier of the branch where the sale occurred.
+        /// </summary>
+        public Guid BranchId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of items included in the sale.
+        /// </summary>
+        public List<CreateSaleItemRequest> Items { get; set; } = new();
+    }
+}
